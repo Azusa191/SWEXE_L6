d9bf3b3c03ef228eba07d0e5b4cb4f0eda98c8e3
Azusa Mori <1911140069x@ed.fuk.kindai.ac.jp>
2021-10-23 15:25:17 +0000

    first commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,11 +8,12 @@ class UsersController < ApplicationController
   end
 
   def create 
-    @user = User.new(uid: params[:user][:uid],pass: params[:user][:pass])
-    if @user.valid? #validatesを実行してくれる
-      #TODO:成功したことを
-      @user.pass = BCrypt::Password.create(params[:user][:pass])
-      @user.save
+    @user = User.new(
+    pass: params[:user][:pass],
+    password: params[:user][:password],
+    password_confirmation: params[:user][:password_confirmation])
+    
+    if @user.save
       redirect_to users_path
     else
       render 'new'
